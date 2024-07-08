3b37afd699c53b4ebec4a0d5d8748325b0ebf16e
Lennin5 <lenninlemus0@gmail.com>
2024-07-08 13:36:49 -0600

    docs(extension vs): agregado de comentarios a código

--- latest_component_changed_vsPackage.cs
+++ latest_component_changed_vsPackage.cs
@@ -10,10 +10,10 @@ using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace VSIXProject1
+namespace LatestComponentChangedVS
 {
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
-    [InstalledProductRegistration("Custom Extension", "Updates status bar with the latest component changed in -gitconfig variable", "1.0")]
+    [InstalledProductRegistration("Custom Extension", "Updates status bar with the latest component changed in -gitconfig variable", "3.0")]
     [ProvideAutoLoad(UIContextGuids80.NoSolution, PackageAutoLoadFlags.BackgroundLoad)]
     [Guid(MyCommandPackage.PackageGuidString)]
     public sealed class MyCommandPackage : AsyncPackage, IVsSolutionEvents
@@ -27,15 +27,19 @@ namespace VSIXProject1
 
         private const string GitConfigPath = @"C:\Users\lennin\.gitconfig"; // Ruta al archivo .gitconfig
 
+        // Método que se ejecuta al inicializar el paquete de forma asíncrona
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
+            // Inicializar el paquete base
             await base.InitializeAsync(cancellationToken, progress);
             await InitializeServicesAsync();
 
+            // Suscribir eventos de solución y comenzar temporizador de actualización de barra de estado
             SubscribeSolutionEvents();
             StartStatusUpdateTimer();
         }
 
+        // Método para inicializar los servicios necesarios
         private async Task InitializeServicesAsync()
         {
             _statusBar = await GetServiceAsync(typeof(SVsStatusbar)) as IVsStatusbar;
@@ -45,19 +49,19 @@ namespace VSIXProject1
             Assumes.Present(_solution);
         }
 
+        // Método para suscribir los eventos de solución
         private void SubscribeSolutionEvents()
         {
-            if (_solution != null)
-            {
-                _solution.AdviseSolutionEvents(this, out _solutionEventsCookie);
-            }
+            _solution?.AdviseSolutionEvents(this, out _solutionEventsCookie);
         }
 
+        // Método para iniciar el temporizador que actualizará la barra de estado cada 1 segundo
         private void StartStatusUpdateTimer()
         {
             _statusUpdateTimer = new System.Threading.Timer(UpdateStatusBar, null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
         }
 
+        // Método que se ejecuta cada vez que el temporizador hace tic para actualizar la barra de estado
         private void UpdateStatusBar(object state)
         {
             ThreadHelper.JoinableTaskFactory.Run(async delegate
@@ -68,12 +72,14 @@ namespace VSIXProject1
             });
         }
 
+        // Método para establecer el mensaje en la barra de estado
         private void SetStatusBarMessage(string message)
         {
             ThreadHelper.ThrowIfNotOnUIThread();
             _statusBar?.SetText(message);
         }
 
+        // Método para obtener el valor de una variable del archivo .gitconfig
         private string GetGitConfigVariableValue(string variableName)
         {
             try
@@ -101,68 +107,19 @@ namespace VSIXProject1
             }
         }
 
-        // Implementación de IVsSolutionEvents
+        // Implementación de los eventos de solución de IVsSolutionEvents
+        public int OnAfterCloseSolution(object pUnkReserved) => VSConstants.S_OK;
+        public int OnAfterLoadProject(IVsHierarchy pStubHierarchy, IVsHierarchy pRealHierarchy) => VSConstants.S_OK;
+        public int OnAfterOpenProject(IVsHierarchy pHierarchy, int fAdded) => VSConstants.S_OK;
+        public int OnAfterOpenSolution(object pUnkReserved, int fNewSolution) => VSConstants.S_OK;
+        public int OnBeforeCloseProject(IVsHierarchy pHierarchy, int fRemoved) => VSConstants.S_OK;
+        public int OnBeforeCloseSolution(object pUnkReserved) => VSConstants.S_OK;
+        public int OnBeforeUnloadProject(IVsHierarchy pRealHierarchy, IVsHierarchy pStubHierarchy) => VSConstants.S_OK;
+        public int OnQueryCloseProject(IVsHierarchy pHierarchy, int fRemoving, ref int pfCancel) => VSConstants.S_OK;
+        public int OnQueryCloseSolution(object pUnkReserved, ref int pfCancel) => VSConstants.S_OK;
+        public int OnQueryUnloadProject(IVsHierarchy pRealHierarchy, ref int pfCancel) => VSConstants.S_OK;
 
-        public int OnAfterCloseSolution(object pUnkReserved)
-        {
-            // No se requiere ninguna acción aquí
-            return VSConstants.S_OK;
-        }
-
-        public int OnAfterLoadProject(IVsHierarchy pStubHierarchy, IVsHierarchy pRealHierarchy)
-        {
-            //SetStatusBarMessage();
-            return VSConstants.S_OK;
-        }
-
-        public int OnAfterOpenProject(IVsHierarchy pHierarchy, int fAdded)
-        {
-            //SetStatusBarMessage();
-            return VSConstants.S_OK;
-        }
-
-        public int OnAfterOpenSolution(object pUnkReserved, int fNewSolution)
-        {
-            //SetStatusBarMessage();
-            return VSConstants.S_OK;
-        }
-
-        public int OnBeforeCloseProject(IVsHierarchy pHierarchy, int fRemoved)
-        {
-            // No se requiere ninguna acción aquí
-            return VSConstants.S_OK;
-        }
-
-        public int OnBeforeCloseSolution(object pUnkReserved)
-        {
-            // No se requiere ninguna acción aquí
-            return VSConstants.S_OK;
-        }
-
-        public int OnBeforeUnloadProject(IVsHierarchy pRealHierarchy, IVsHierarchy pStubHierarchy)
-        {
-            // No se requiere ninguna acción aquí
-            return VSConstants.S_OK;
-        }
-
-        public int OnQueryCloseProject(IVsHierarchy pHierarchy, int fRemoving, ref int pfCancel)
-        {
-            // No se requiere ninguna acción aquí
-            return VSConstants.S_OK;
-        }
-
-        public int OnQueryCloseSolution(object pUnkReserved, ref int pfCancel)
-        {
-            // No se requiere ninguna acción aquí
-            return VSConstants.S_OK;
-        }
-
-        public int OnQueryUnloadProject(IVsHierarchy pRealHierarchy, ref int pfCancel)
-        {
-            // No se requiere ninguna acción aquí
-            return VSConstants.S_OK;
-        }
-
+        // Método para liberar recursos: temporizador y eventos de solución
         protected override void Dispose(bool disposing)
         {
             if (disposing)
@@ -173,6 +130,7 @@ namespace VSIXProject1
             base.Dispose(disposing);
         }
 
+        // Método para desuscribir los eventos de solución: IVsSolutionEvents
         private void UnsubscribeSolutionEvents()
         {
             if (_solution != null && _solutionEventsCookie != 0)
